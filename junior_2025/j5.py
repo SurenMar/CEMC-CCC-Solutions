03479b08fd860cd66be9c4fc37c05833f9862b57
SurenMar <suren.v.mar@gmail.com>
2025-05-13 22:22:24 -0400

    added 2025 j5

---
+++ junior_2025/j5.py
@@ -0,0 +1,43 @@
+rows = int(input())
+cols = int(input())
+m = int(input())
+
+# create grid
+grid = []
+n = 1
+for i in range(rows):
+    # create row
+    row = []
+    for j in range(cols):
+        row.append(n)
+        n += 1
+        # reset n
+        if n > m:
+            n = 1
+    grid.append(row)    # add row to grid
+
+# recursive function to find optimal path given a row and col
+def find_path(row, col):
+    # base case
+    if row == rows - 1:
+        return grid[row][col]
+    # left most column on the grid
+    elif col == 0:
+        return grid[row][col] + min(find_path(row + 1, col),
+                                    find_path(row + 1, col + 1))
+    # right most column on the grid
+    elif col == cols - 1:
+        return grid[row][col] + min(find_path(row + 1, col - 1),
+                                    find_path(row + 1, col))
+    # middle of the grid
+    else:
+        return grid[row][col] + min(find_path(row + 1, col - 1),
+                                    find_path(row + 1, col),
+                                    find_path(row + 1, col + 1))
+        
+# find optimal path among all starting points on first row
+min_path = find_path(0, 0)
+for col in range(1, cols):
+    min_path = min(min_path, find_path(0, col))
+    
+print(min_path)
