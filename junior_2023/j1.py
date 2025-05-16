613eca6229f63892c532c355a23993a00cb77d0d
SurenMar <suren.v.mar@gmail.com>
2025-05-16 13:36:45 -0400

    Added 2023 j1-3

---
+++ junior_2023/j1.py
@@ -0,0 +1,7 @@
+p = int(input())
+c = int(input())
+
+if p > c:
+    print(500 + p * 50 - c * 10)
+else:
+    print(p * 50 - c * 10)
---
+++ junior_2023/j2.py
@@ -0,0 +1,16 @@
+# Peppers dictionary using first letters to distinguish between peppers since
+#   all first letters are distinct
+peppers = {
+    'P': 1500,
+    'M': 6000,
+    'S': 15500,
+    'C': 40000,
+    'T': 75000,
+    'H': 125000
+}
+
+total = 0
+for i in range(int(input())):
+    total += peppers[input()[0]]    # Access dictionary using first character
+                                    #   of inputted name
+print(total)
