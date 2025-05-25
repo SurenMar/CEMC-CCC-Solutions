7b385192066c9b0852e3265e21ed807b6cfddbf2
SurenMar <suren.v.mar@gmail.com>
2025-05-25 13:45:04 -0400

    Added 2020 j1-3

---
+++ junior_2020/j1.py
@@ -0,0 +1,6 @@
+# https://cemc.uwaterloo.ca/sites/default/files/documents/2020/2020CCCJrProblemSet.html
+
+if int(input()) + 2 * int(input()) + 3 * int(input()) >= 10:
+    print('happy')
+else:
+    print('sad')
