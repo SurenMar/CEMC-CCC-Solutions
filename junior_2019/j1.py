7f8a0ec323fc305f98a233db12ecd28bd7bc7598
SurenMar <suren.v.mar@gmail.com>
2025-05-28 14:23:08 -0400

    Added 2019 j1-3

---
+++ junior_2019/j1.py
@@ -0,0 +1,11 @@
+# https://cemc.uwaterloo.ca/sites/default/files/documents/2019/2019CCCJrProblemSet.html
+
+a = int(input()) * 3 + int(input()) * 2 + int(input()) * 1
+b = int(input()) * 3 + int(input()) * 2 + int(input()) * 1
+
+if a > b:
+    print('A')
+elif a < b:
+    print('B')
+else:
+    print('T')
