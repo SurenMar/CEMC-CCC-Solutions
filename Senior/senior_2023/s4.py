8f57164f65f2285c4b5971b5a5214073a5e0b685
SurenMar <suren.v.mar@gmail.com>
2025-07-22 14:35:19 -0400

    Added 2023 s4

--- Senior/senior_2023/s4.py
+++ Senior/senior_2023/s4.py
@@ -1,48 +1,71 @@
 # https://cemc.uwaterloo.ca/sites/default/files/documents/2023/2023CCCSrProblems.html
 
-# You will need to track visited squares as well as have some sort of path
-#   length counter that will keep track of the length of a path back to a 
-#   certain point in the search
-
-# When deciding what path to visit from a set of new ones, pick the shortest
-#   one, then the lowest cost
-
-# Used to insert elements in a list while keeping the list sorted
-from bisect import bisect_left
-
 num_nodes, num_paths = map(int, input().split())
 
-# Read graph
-graph = [[] for _ in range(num_nodes)]
+# Store all roads as tuples: (length, cost, node1, node2)
+edges = []
 for _ in range(num_paths):
-    node1, node2, length, cost = map(int, input().split())
-    graph[node1].append((node2, length, cost))
+    u, v, l, c = map(int, input().split())
+    edges.append((l, c, u - 1, v - 1))  # convert to 0-based indexing
 
-# Finds the shortest path that can be taken.
-def shortest_path(possible_paths):
-    min_idx = 0
-    for i in range(1, len(possible_paths)):
-        if possible_paths[i][1] < possible_paths[min_idx][1]:
-            min_idx = i
-    return min_idx
-        
-# Recursive function to find most optimal path in graph
-def find_path(node, num_visited, possible_paths, total_cost, prefix_length):
-    """
-    num_visted: number of all visited paths
-    possible_paths: unique list of all current paths that can be taken
-        element fromat: (destination_node, path_length, cost)
-    total_cost: total cost of maintaining the chosen paths
-    prefix_length: hashmap of prefix sums of path lengths
-    """
-    # Base case
-    if num_visited == num_nodes:
-        return total_cost
-    else:
-        # Update possible paths
-        for neighbours in graph[node]:
-            possible_paths.append(neighbours)
-        # Remove duplicates and find shortest path
-        possible_paths = list(set(possible_paths))
-        min_idx = shortest_path(possible_paths)
-          
+# Sort by shortest length first, then by lowest cost
+edges.sort()
+
+# Graph of currently chosen roads (those that maintain shortest paths)
+# Each entry: [ (neighbor_node, road_length), ... ]
+graph = [[] for _ in range(num_nodes)]
+
+# Tracks total cost of maintaining roads required for shortest paths
+total_cost = 0
+
+# Function to find the shortest path from start to end using current roads
+# Only uses the roads that are in 'graph'
+def shortest_distance(start, end, limit):
+    visited = [False] * num_nodes
+    distances = [float('inf')] * num_nodes
+    distances[start] = 0
+
+    # Simple queue (inefficient but acceptable for small graphs)
+    queue = [start]
+
+    while queue:
+        # Find the node in the queue with the smallest distance
+        min_idx = 0
+        for i in range(1, len(queue)):
+            if distances[queue[i]] < distances[queue[min_idx]]:
+                min_idx = i
+
+        node = queue.pop(min_idx)
+        visited[node] = True
+
+        # Early stop if we go past limit
+        if distances[node] > limit:
+            break
+
+        # Stop if we reached destination
+        if node == end:
+            return distances[node]
+
+        # Explore neighbors
+        for neighbor, length in graph[node]:
+            if not visited[neighbor] and distances[node] + length < distances[neighbor]:
+                distances[neighbor] = distances[node] + length
+                # Add to queue if not already there
+                if neighbor not in queue:
+                    queue.append(neighbor)
+
+    return distances[end]
+
+# Go through each road, shortest first
+for length, cost, u, v in edges:
+    # Check if current shortest path between u and v is greater than this road
+    current = shortest_distance(u, v, length)
+
+    # If it's longer, we must add this road to maintain shortest paths
+    if current > length:
+        graph[u].append((v, length))
+        graph[v].append((u, length))
+        total_cost += cost
+
+# Output
+print(total_cost)
