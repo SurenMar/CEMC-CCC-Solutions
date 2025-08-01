d53ead1fde026a16295608591fc5f730fb11e919
Suren Martirosyan <suren.v.mar@gmail.com>
2025-08-01 12:35:26 -0400

    Added 2021 s1

---
+++ Senior/senior_2021/s1.py
@@ -0,0 +1,18 @@
+# https://cemc.uwaterloo.ca/sites/default/files/documents/2022/2022CCCSrProblems.html
+
+num_wood = int(input())
+heights = list(map(int, input().split()))
+widths = list(map(int, input().split()))
+
+area = 0
+# Loop through all pieces of wood for fencing
+for i in range(num_wood):
+    min_height = min(heights[i], heights[i+1])
+    max_height = max(heights[i], heights[i+1])
+    # Calculate area of rectangle and traingel seperately
+    rectangle = min_height * widths[i]
+    triangle = (max_height - min_height) * widths[i] / 2
+    area += rectangle + triangle
+
+# Output
+print(area)
