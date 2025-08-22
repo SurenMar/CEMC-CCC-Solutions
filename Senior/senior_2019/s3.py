50dd3f03647efb5916f841f7920619bb601db271
Suren Martirosyan <suren.v.mar@gmail.com>
2025-08-22 18:14:17 -0400

    Added 2019 s3

--- Senior/senior_2019/s3.py
+++ Senior/senior_2019/s3.py
@@ -2,8 +2,15 @@
 
 square = [list(input().split()) for _ in range(3)]
 
-new_square = [[], [], []]
+# Deep copies the square
+def deep_copy(square, new_square):
+    for i in range(3):
+        new_square[i][0] = square[i][0]
+        new_square[i][1] = square[i][1]
+        new_square[i][2] = square[i][2]
 
+
+# Finds and fills in the missing term in a row or column
 def find_missing_term(missing_idx, lst):
     if missing_idx == 0:
         val1, val2 = int(lst[1]), int(lst[2])
@@ -16,23 +23,57 @@ def find_missing_term(missing_idx, lst):
         return val2 - val1 + val2
 
 # Find rows with only 1 X
-for row in square:
-    if row.count('X') == 1:
-        i = row.index('X')
-        row[i] = str(find_missing_term(i, row))
+def fill_row(square):
+    for row in square:
+        if row.count('X') == 1:
+            i = row.index('X')
+            row[i] = str(find_missing_term(i, row))
 
 # Same thing but for columns
-square = list(map(list, zip(*square)))  # Transpose square
-for col in square:
-    if col.count('X') == 1:
-        i = col.index('X')
-        col[i] = str(find_missing_term(i, col))
+def fill_col(square):
+    square = list(map(list, zip(*square)))  # Transpose square
+    for col in square:
+        if col.count('X') == 1:
+            i = col.index('X')
+            col[i] = str(find_missing_term(i, col))
+    square = list(map(list, zip(*square)))  # Revert square back
 
-square = list(map(list, zip(*square)))  # Revert square back
-print(square)
+new_square = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# Fill rows and columns until square is complete
+while 'X' in square[0] or 'X' in square[1] or 'X' in square[2]:
+    deep_copy(square, new_square)
+    fill_row(square)
+    fill_col(square)
+    # Check if square has no rows and columns with at least 2 numbers
+    if square == new_square:
+        print('here')
+        # Check rows
+        for i, row in enumerate(square):
+            elem = 0
+            for item in row:
+                if item != 'X':
+                    elem = item
+                    break
+            square[i] = [elem] * 3
+            print(elem, row)
+        # Check columns
+    elif square == new_square:
+        print('here2')
+        square = list(map(list, zip(*square)))  # Transpose square
+        for i, col in enumerate(square):
+            elem = 0
+            for item in col:
+                if item != 'X':
+                    elem = item
+                    break
+            square[i] = [elem] * 3
+        square = list(map(list, zip(*square)))  # Revert square back
+    print('-------------')
+    print(square)
+    print(new_square)
 
-# NOTE: THIS SOLUTION DOESNT WORK WITH:
-# X X 1
-# 2 2 2
-# 4 6 3
-# (top row not a sequence)
+# Output
+for row in square:
+    for elem in row:
+        print(elem, end=' ')
+    print()
