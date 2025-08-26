b651cb2538e71c0af08a711281269d7237877534
Suren Martirosyan <suren.v.mar@gmail.com>
2025-08-25 23:21:18 -0400

    Added 2019 s4

--- Senior/senior_2019/s4.py
+++ Senior/senior_2019/s4.py
@@ -2,40 +2,27 @@
 
 num_dests, max_dests = map(int, input().split())
 dest_scores = list(map(int, input().split()))
-
 leftover = num_dests % max_dests
 
-# Group the scores into groups of length max_dests
-# There will be one group at the end with <max_dests length
-# Arrange this arrangement by moving the last group aroun and seeing which
-#   arrangement has the highest total score
-
-# (1 2 3) (4 5 6) (7 8)
-# (1 2 3) (4 5) (6 7 8)
-# (1 2) (3 4 5) (6 7 8)
-
-# Caclulates score of a specific group in lst
-def calc_score(lst, start, group_len):
-    i = start
-    score = 0
-    while i < start + group_len:
-        score += lst[i]
-    return score
-
-# Add the remaining group and calculate current total score
+# Calculate initial score
 score = 0
-high = num_dests - 1
-low = num_dests - leftover - max_dests
+groups = [max_dests] * (num_dests // max_dests)
+groups.append(leftover) if leftover == 0 else None
+for i, group in enumerate(groups):
+    score += max(dest_scores[i * max_dests : (i+1) * max_dests])
 # Output total score if there is no left over group
-if len(leftover == 0):
+if leftover == 0:
     print(score)
     quit() 
 # Shuffle the leftover group and find max score of each arrangement
 else:
-    high = num_dests - 1
+    high = num_dests
     low = num_dests - leftover - max_dests
-    while True:
-        # Calculate current score
+    while low >= 0:
+        score += max(dest_scores[low : high])
+        low -= max_dests
+        high -= max_dests
 
-        score = max(score, )
+# Output
+print(score)
 
